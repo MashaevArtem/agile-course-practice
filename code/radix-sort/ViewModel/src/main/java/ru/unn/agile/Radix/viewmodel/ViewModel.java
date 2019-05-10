e9a1285a88918bdd3b4df9a27f7007a655d75c01
Artem <timofeynn@mail.ru>
2019-05-10 20:25:10 +0300

    revert 3

--- code/radix-sort/ViewModel/src/main/java/ru/unn/agile/Radix/viewmodel/ViewModel.java
+++ code/radix-sort/ViewModel/src/main/java/ru/unn/agile/Radix/viewmodel/ViewModel.java
@@ -5,8 +5,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import ru.unn.agile.Radix.model.RadixSort;
-import ru.unn.agile.Radix.model.RadixNode;
+import ru.unn.agile.Radix.Model.RadixSort;
+import ru.unn.agile.Radix.Model.RadixNode;
 
 public class ViewModel 
 {
@@ -50,7 +50,7 @@ public class ViewModel
      {
         try 
         {
-        RadixNode<Integer, String> found = tree.find(Integer.parseInt(key.get()));
+        RBNode<Integer, String> found = tree.find(Integer.parseInt(key.get()));
 
         if (found != null) 
         {
