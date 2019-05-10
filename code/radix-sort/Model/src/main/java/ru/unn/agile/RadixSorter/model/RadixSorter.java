4e0b64ca44cc63a5c4dc61a74f94a9e0c9a8cd0f
Artem <timofeynn@mail.ru>
2019-05-10 23:03:29 +0300

    new try 5

--- code/radix-sort/Model/src/main/java/ru/unn/agile/RadixSorter/model/RadixSorter.java
+++ code/radix-sort/Model/src/main/java/ru/unn/agile/RadixSorter/model/RadixSorter.java
@@ -1,9 +1,10 @@
+package ru.unn.agile.radixsorter.model;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-package ru.unn.agile.radixsorter.model;
 
 public class RadixSorter {
 
