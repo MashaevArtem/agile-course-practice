a3fb424663c4b2ff47961e0e1865a06689b809a2
Artem <timofeynn@mail.ru>
2019-05-10 08:09:24 +0300

    Add ViewModel

--- code/radix-sort/ViewModel/src/test/java/ru/unn/agile/Radix/viewmodel/ViewModelTests.java
+++ code/radix-sort/ViewModel/src/test/java/ru/unn/agile/Radix/viewmodel/ViewModelTests.java
@@ -1,169 +1,93 @@
-package ru.unn.agile.ComplexNumber.viewmodel;
+package ru.unn.agile.Radix.ViewModel;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import ru.unn.agile.ComplexNumber.model.ComplexNumber.Operation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-public class ViewModelTests {
+public class ViewModelTests 
+   {
+
     private ViewModel viewModel;
 
     @Before
-    public void setUp() {
+    public void setUp() 
+    {
         viewModel = new ViewModel();
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() 
+    {
         viewModel = null;
     }
 
     @Test
-    public void canSetDefaultValues() {
-        assertEquals("", viewModel.re1Property().get());
-        assertEquals("", viewModel.im1Property().get());
-        assertEquals("", viewModel.re2Property().get());
-        assertEquals("", viewModel.im2Property().get());
-        assertEquals(Operation.ADD, viewModel.operationProperty().get());
-        assertEquals("", viewModel.resultProperty().get());
-        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    public void isKeyEmptyByDefault() 
+    {
+        assertEquals("", viewModel.keyProperty().get());
     }
 
     @Test
-    public void statusIsWaitingWhenCalculateWithEmptyFields() {
-        viewModel.calculate();
-        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    public void isValueEmptyByDefault() 
+    {
+        assertEquals("", viewModel.valueProperty().get());
     }
 
     @Test
-    public void statusIsReadyWhenFieldsAreFill() {
-        viewModel.re1Property().set("1");
-        viewModel.im1Property().set("2");
-        viewModel.re2Property().set("3");
-        viewModel.im2Property().set("4");
-
-        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    public void isStatusEmptyByDefault() 
+    {
+        assertEquals(ViewModel.Status.WAITING_FOR_INPUT, viewModel.status());
     }
 
     @Test
-    public void canReportBadFormat() {
-        viewModel.re1Property().set("a");
-
-        assertEquals(Status.BAD_FORMAT.toString(), viewModel.statusProperty().get());
+    public void insertingANode() 
+    {
+        viewModel.keyProperty().setValue("23456");
+        viewModel.valueProperty().setValue("qwerty");
+        viewModel.actionInsert();
+        assertEquals(ViewModel.Status.SUCCESS, viewModel.status());
     }
 
     @Test
-    public void statusIsWaitingIfNotEnoughCorrectData() {
-        viewModel.re1Property().set("1");
-
-        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    public void insertingBadKeyFormat() 
+    {
+        viewModel.keyProperty().setValue("      567");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        assertEquals(ViewModel.Status.BAD_KEY_FORMAT, viewModel.status());
     }
 
     @Test
-    public void calculateButtonIsDisabledInitially() {
-        assertTrue(viewModel.calculationDisabledProperty().get());
+    public void findANode() 
+   {
+        viewModel.keyProperty().setValue("567");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.SUCCESS, viewModel.status());
     }
 
     @Test
-    public void calculateButtonIsDisabledWhenFormatIsBad() {
-        setInputData();
-        viewModel.re1Property().set("trash");
-
-        assertTrue(viewModel.calculationDisabledProperty().get());
+    public void nodeNotFound() 
+    {
+        viewModel.keyProperty().setValue("567");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.keyProperty().setValue("1");
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.NOT_FOUND, viewModel.status());
     }
 
     @Test
-    public void calculateButtonIsDisabledWithIncompleteInput() {
-        viewModel.re1Property().set("1");
-
-        assertTrue(viewModel.calculationDisabledProperty().get());
-    }
-
-    @Test
-    public void calculateButtonIsEnabledWithCorrectInput() {
-        setInputData();
-
-        assertFalse(viewModel.calculationDisabledProperty().get());
-    }
-
-    @Test
-    public void canSetAddOperation() {
-        viewModel.operationProperty().set(Operation.ADD);
-        assertEquals(Operation.ADD, viewModel.operationProperty().get());
-    }
-
-    @Test
-    public void addIsDefaultOperation() {
-        assertEquals(Operation.ADD, viewModel.operationProperty().get());
-    }
-
-    @Test
-    public void operationAddHasCorrectResult() {
-        viewModel.re1Property().set("1");
-        viewModel.im1Property().set("4");
-        viewModel.re2Property().set("-2");
-        viewModel.im2Property().set("-2.5");
-
-        viewModel.calculate();
-
-        assertEquals("-1.0 + 1.5i", viewModel.resultProperty().get());
-    }
-
-    @Test
-    public void canSetSuccessMessage() {
-        setInputData();
-
-        viewModel.calculate();
-
-        assertEquals(Status.SUCCESS.toString(), viewModel.statusProperty().get());
-    }
-
-    @Test
-    public void canSetBadFormatMessage() {
-        viewModel.re1Property().set("#selfie");
-
-        assertEquals(Status.BAD_FORMAT.toString(), viewModel.statusProperty().get());
-    }
-
-    @Test
-    public void statusIsReadyWhenSetProperData() {
-        setInputData();
-
-        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
-    }
-
-    @Test
-    public void operationMulHasCorrectResult() {
-        viewModel.re1Property().set("2");
-        viewModel.im1Property().set("3");
-        viewModel.re2Property().set("1");
-        viewModel.im2Property().set("2");
-        viewModel.operationProperty().set(Operation.MULTIPLY);
-
-        viewModel.calculate();
-
-        assertEquals("-4.0 + 7.0i", viewModel.resultProperty().get());
-    }
-
-    @Test
-    public void operationAddWithNegativeNumbersHasCorrectResult() {
-        viewModel.re1Property().set("1.2");
-        viewModel.im1Property().set("2.3");
-        viewModel.re2Property().set("-10.4");
-        viewModel.im2Property().set("-20.5");
-        viewModel.operationProperty().set(Operation.ADD);
-
-        viewModel.calculate();
-
-        assertEquals("-9.2 - 18.2i", viewModel.resultProperty().get());
-    }
-
-    private void setInputData() {
-        viewModel.re1Property().set("1");
-        viewModel.im1Property().set("2");
-        viewModel.re2Property().set("3");
-        viewModel.im2Property().set("4");
+    public void findingBadKeyFormat() 
+    {
+        viewModel.keyProperty().setValue("567");
+        viewModel.valueProperty().setValue("");
+        viewModel.actionInsert();
+        viewModel.keyProperty().setValue("    1");
+        viewModel.actionFind();
+        assertEquals(ViewModel.Status.BAD_KEY_FORMAT, viewModel.status());
     }
 }
