2123c697ba9849c2d3b99ac947eae67884facd5d
Artem <timofeynn@mail.ru>
2019-05-10 16:33:57 +0300

    Add more v2

--- code/radix-sort/View/src/main/java/ru/unn/agile/Radix/View/Radix.java
+++ code/radix-sort/View/src/main/java/ru/unn/agile/Radix/View/Radix.java
@@ -1,45 +1,55 @@
-package ru.unn.agile.ComplexNumber.view;
+package ru.unn.agile.Radix.View;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import ru.unn.agile.ComplexNumber.model.ComplexNumber.Operation;
-import ru.unn.agile.ComplexNumber.viewmodel.ViewModel;
+import ru.unn.agile.Radix.ViewModel.ViewModel;
 
-public class Calculator {
+public class Radix 
+{
     @FXML
     private ViewModel viewModel;
-    @FXML
-    private TextField txtZ1Re;
-    @FXML
-    private TextField txtZ1Im;
-    @FXML
-    private TextField txtZ2Re;
-    @FXML
-    private TextField txtZ2Im;
-    @FXML
-    private ComboBox<Operation> cbOperation;
-    @FXML
-    private Button btnCalc;
 
     @FXML
-    void initialize() {
+    private Button performInsertion;
+	
+    @FXML
+    private Button performFind;
+	
+    @FXML
+    private TextField key;
+	
+    @FXML
+    private TextField value;
+	
+    @FXML
+    private Label status;
 
-        // Two-way binding hasn't supported by FXML yet, so place it in code-behind
-        txtZ1Re.textProperty().bindBidirectional(viewModel.re1Property());
-        txtZ1Im.textProperty().bindBidirectional(viewModel.im1Property());
-        txtZ2Re.textProperty().bindBidirectional(viewModel.re2Property());
-        txtZ2Im.textProperty().bindBidirectional(viewModel.im2Property());
+    @FXML
+    void initialize() 
+	{
+        key.textProperty().bindBidirectional(viewModel.keyProperty());
+        value.textProperty().bindBidirectional(viewModel.valueProperty());
+        status.textProperty().bind(viewModel.statusProperty().asString());
 
-        cbOperation.valueProperty().bindBidirectional(viewModel.operationProperty());
-
-        btnCalc.setOnAction(new EventHandler<ActionEvent>() {
+        performFind.setOnAction(new EventHandler<ActionEvent>() 
+		{
             @Override
-            public void handle(final ActionEvent event) {
-                viewModel.calculate();
+            public void handle(final ActionEvent event) 
+			{
+                viewModel.actionFind();
+            }
+        });
+
+        performInsertion.setOnAction(new EventHandler<ActionEvent>() 
+		{
+            @Override
+            public void handle(final ActionEvent event) 
+			{
+                viewModel.actionInsert();
             }
         });
     }
