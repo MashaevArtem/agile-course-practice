94ca2924540a517834af8531713b518f18ebe40a
Artem <timofeynn@mail.ru>
2019-05-11 08:07:40 +0300

    new try 22

--- code/radix-sort/ViewModel/src/main/java/ru/unn/agile/RadixSorter/viewmodel/ViewModel.java
+++ code/radix-sort/ViewModel/src/main/java/ru/unn/agile/RadixSorter/viewmodel/ViewModel.java
@@ -73,7 +73,6 @@ public class ViewModel {
 
     public void sort() {
         int[] nativeArray = new int[sortedArray.size()];
-        sortedArray.mapToInt(i->i).toArray(nativeArray);
 
         RadixSorter.sort(nativeArray);
 
