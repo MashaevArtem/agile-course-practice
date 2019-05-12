74d0c2fb1efc19b571c3895cd0cb8bcc8db600c8
Artem <timofeynn@mail.ru>
2019-05-12 19:50:44 +0300

    add model + view + viewmodel

---
+++ code/radix-sort/Model/src/main/java/ru/unn/agile/RadixSorter/Model/RadixSorter.java
@@ -0,0 +1,76 @@
+package ru.unn.agile.RadixSorter.model;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public final class RadixSorter {
+
+    private RadixSorter() {
+    }
+
+    public static final int DEFAULT_RADIX = 10;
+
+    public static void sort(final int[] arr) {
+        sort(arr, DEFAULT_RADIX);
+    }
+
+    public static void sort(final int[] arr, final int radix) {
+        int maxDigits = 1 + (int) (Math.log(max(arr)) / Math.log(radix));
+        int div = 1;
+        for (int pos = 0; pos < maxDigits; ++pos) {
+            List<List<Integer>> buckets = splitToBuckets(arr, div, radix);
+            flattenBuckets(arr, buckets);
+            div *= radix;
+        }
+        List<List<Integer>> buckets = splitBySign(arr);
+        flattenBuckets(arr, buckets);
+    }
+
+private static List<List<Integer>> splitToBuckets(final int[] arr, final int div, final int radix) {
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (int i = 0; i < radix; ++i) {
+            buckets.add(new LinkedList<>());
+        }
+        for (int num : arr) {
+            int bucketIndex = Math.abs(num) / div % radix;
+            buckets.get(bucketIndex).add(num);
+        }
+        return buckets;
+    }
+
+    private static List<List<Integer>> splitBySign(final int[] arr) {
+        List<Integer> positive = new LinkedList<>();
+        List<Integer> negative = new LinkedList<>();
+        for (int num : arr) {
+            if (num >= 0) {
+                positive.add(num);
+            } else {
+                negative.add(0, num);
+            }
+        }
+        return Arrays.asList(negative, positive);
+    }
+
+ private static void flattenBuckets(final int[] arr, final List<? extends List<Integer>> buckets) {
+        int i = 0;
+        for (List<Integer> bucket : buckets) {
+            for (int num : bucket) {
+                arr[i++] = num;
+            }
+        }
+    }
+
+    private static int max(final int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+}
---
+++ code/radix-sort/ViewModel/src/main/java/ru/unn/agile/RadixSorter/ViewModel/ViewModel.java
@@ -0,0 +1,149 @@
+package ru.unn.agile.RadixSorter.viewmodel;
+
+import ru.unn.agile.RadixSorter.model.RadixSorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewModel {
+    private String inputValue;
+    private String sortedArrayStringRepresentation;
+    private String inputArrayStringRepresentation;
+    private String status;
+
+    private boolean isAdButtonEnabled;
+    private boolean isCleanButtonEnabled;
+    private boolean isSortButtonEnabled;
+
+    private List<Integer> sortedArray = new ArrayList<Integer>();
+    private List<Integer> inputArray = new ArrayList<Integer>();
+
+    public ViewModel() {
+        inputValue = "";
+        sortedArrayStringRepresentation = "";
+        inputArrayStringRepresentation = "";
+        status = Status.WAITING;
+
+        isAdButtonEnabled = false;
+        isCleanButtonEnabled = false;
+        isSortButtonEnabled = false;
+    }
+
+    public String getSortedArrayStringRepres() {
+        return sortedArrayStringRepresentation;
+    }
+
+    public String getInputArrayStringRepresentation() {
+        return inputArrayStringRepresentation;
+    }
+
+    public String getElemArray() {
+        return inputValue;
+    }
+
+    public void setInputValue(final String inputValue) {
+        if (inputValue.equals(this.inputValue)) {
+            return;
+        }
+
+        this.inputValue = inputValue;
+    }
+
+    public void addProcess() {
+        if (!parseInput()) {
+            return;
+        }
+
+        int value = Integer.parseInt(inputValue);
+        sortedArray.add(value);
+        inputArray.add(value);
+        changeButtonsEnabling();
+
+        inputArrayStringRepresentation = sortedArray.toString();
+    }
+
+    public void clearProcess() {
+        sortedArray.clear();
+        inputArray.clear();
+        changeButtonsEnabling();
+
+        sortedArrayStringRepresentation = sortedArray.toString();
+        inputArrayStringRepresentation = inputArray.toString();
+    }
+
+    public void sort() {
+        int[] nativeArray = new int[sortedArray.size()];
+
+
+        RadixSorter.sort(nativeArray);
+
+        for (int i = 0; i < nativeArray.length; ++i) {
+            sortedArray.set(i, nativeArray[i]);
+        }
+
+        changeButtonsEnabling();
+        status = Status.SUCCESSFUL;
+
+        sortedArrayStringRepresentation =  sortedArray.toString();
+    }
+
+    public String getCurrentState() {
+        return status;
+    }
+
+    public final class Status {
+        public static final String WAITING = "Waiting new element";
+        public static final String READY = "Ready to add new element";
+        public static final String BAD_FORMAT = "Bad format";
+        public static final String SUCCESSFUL = "Sort of array is successful";
+
+        private Status() { }
+    }
+
+    public void processingAddField(final int keyCode) {
+        parseInput();
+    }
+
+    private boolean parseInput() {
+
+        if (inputValue.isEmpty()) {
+            status = Status.WAITING;
+            isAdButtonEnabled = false;
+            return isAdButtonEnabled;
+        }
+
+        try {
+            Integer.parseInt(inputValue);
+            status = Status.READY;
+            isAdButtonEnabled = true;
+        } catch (Exception e) {
+            status = Status.BAD_FORMAT;
+            isAdButtonEnabled = false;
+            return isAdButtonEnabled;
+        }
+
+        return isAdButtonEnabled;
+    }
+
+    public boolean isAdButtonEnabled() {
+        return isAdButtonEnabled;
+    }
+
+    public boolean isCleanButtonEnabled() {
+        return isCleanButtonEnabled;
+    }
+
+    public boolean isSortButtonEnabled() {
+        return isSortButtonEnabled;
+    }
+
+    private void changeButtonsEnabling() {
+        if (sortedArray.isEmpty()) {
+            isSortButtonEnabled = false;
+            isCleanButtonEnabled = false;
+        } else {
+            isSortButtonEnabled = true;
+            isCleanButtonEnabled = true;
+        }
+    }
+}
