b387deaa3dcbb3eb5cef7bf1e67a3bd697ed5507
Artem <timofeynn@mail.ru>
2019-05-12 16:05:23 +0300

    final2

--- code/radix-sort/Model/src/test/java/ru/unn/agile/RadixSorter/Model/RadixSorterTest.java
+++ code/radix-sort/Model/src/test/java/ru/unn/agile/RadixSorter/Model/RadixSorterTest.java
@@ -1,6 +1,6 @@
-package ru.unn.agile.radixsorter.model;
-import org.junit.Test;
+package ru.unn.agile.RadixSorter.model;
 
+import org.junit.Test;
 import java.util.Arrays;
 import java.util.Random;
 
